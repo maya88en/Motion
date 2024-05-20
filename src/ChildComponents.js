c95af2a50698fe16d6dcbe6ec284766adbee5adb
maya88en <maya88en@gmail.com>
2024-05-20 05:26:43 +0300

    add project

---
+++ src/ChildComponents.js
@@ -0,0 +1,159 @@
+import React from 'react';
+
+const ChildComponent1 = () => {
+  return <div class="container">
+  <nav class="navbar fixed-top bg-dark">
+  <a class="navbar-brand" href="https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891?_gl=1*laf8gl*_ga*MTg2ODQyMjM2NC4xNzE2MTA1NzI1*_ga_ZKBVC1X78F*MTcxNjE2OTM4OC4yLjEuMTcxNjE2OTU5NS4wLjAuMA..&_ga=2.219356891.311114945.1716105725-1868422364.1716105725">
+    <img src="./market.svg" alt="logo" height={100} width={150}></img>
+  </a>
+  <ul class="nav">
+    <li class="nav-item">
+      <p class="nav-link"></p>
+    </li>
+    <li class="nav-item">
+      <p class="nav-link"></p>
+    </li>
+    <li class="nav-item ">
+      <a class="nav-link" href="https://codecanyon.net/checkout/102302114/create_account?_ga=2.4797297.311114945.1716105725-1868422364.1716105725">
+      <button type="button" class="btn btn-success">Buy Now</button>
+      </a>
+    </li>
+  </ul>
+</nav>
+</div>;
+};
+
+const ChildComponent2 = () => {
+  return <div class="container">
+  <div class="row">
+    <div class="col-sm">
+        <div className='logo'>
+            <a href="www.google.com">
+            <img src="./MotionArtEffect-logo.png" alt="logo" height={50} width={200}></img>
+            </a>
+        </div>
+        <div class="">
+            <h5 className='mb-4'>Transform Your Website </h5> 
+            <h6>With Motion Art Effect</h6>  
+        </div>
+    </div>
+    <div class="col-sm">
+    </div>
+    <div class="col-sm">
+        <div className='logo'>
+        </div>
+        <div class="">
+            <h1 class='mb-4'> Attract Your Visitors Attention With ColorfulMotion Art Effect </h1> 
+            <p class='mb-5'>Unleash the power of creativity with Motion Art for Elementor - your ultimate solution for seamlessly integrating captivating animations into your website. </p> 
+            <h4 class='mb-5'>Trusted by thousands of users around the world</h4> 
+        </div>
+      
+    </div>
+
+        <div class="col-sm">
+            <a href='https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891'>
+            <button type="button" class="btn btn-light">Purchase Now</button>
+            </a>
+        </div>
+  </div>
+        <div class="row mb-5">
+            <div class="col-sm">
+            <img className='mb-4' src="./effect2.png" alt="effect1" height={100} width={100}></img>
+            <div className='star'>
+                <img width="155" height="20" srcset="https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img4-150x20.png" alt="stars-img"></img>
+                <p>4.5 Score, 9 Reviews</p>
+            </div>
+            </div>
+            <div class="col-sm">
+            <img className='mb-4' src="./effect1.png" alt="effect1" height={100} width={100}></img>
+            <div className='star'>
+                <img width="155" height="20" srcset="https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img4-150x20.png" alt="stars-img"></img>
+                <p>4.5 Score, 9 Reviews</p>
+            </div>
+            </div>
+            <div class="col-sm">
+            <img class='mb-4' src="./effect3.png" alt="effect1" height={100} width={100}></img>
+            <div class='star'>
+                <img width="155" height="20" srcset="https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img4-150x20.png" alt="stars-img"></img>
+                <p>4.5 Score, 9 Reviews</p>
+            </div>
+            </div>
+        </div>
+        <div class='row'>
+            <div class="col-sm-8">
+                <h2 class='mb-4'>Turn Your Cursor Into A Colorful Magic Wand & Charm Your Visitors</h2>
+                <p class="transparant">Motion Art for Elementor is a groundbreaking plugin that empowers you to effortlessly infuse your website with visually stunning motion art elements.</p>
+                <a href='https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891'>
+                <button type="button" class="btn envato">Purchase From Envato</button>
+                </a>
+            </div>
+            <div class="col-sm-4">
+            <img class='mb-4' src="./effect4.png" alt="effect4" height={250} width={200}></img>
+            </div>
+        </div>
+        <h2 class="text-center mb-5">Apply On Any Section Or Enable For Whole Page</h2>
+        <div class="row">
+            <div class="col-sm-6 rounded apply">
+            <h3>Apply On Section</h3>
+            <p class="transparant">Apply on section is a game-changer, offering an unparalleled way to manage applications directly from your website.</p>
+            <img class='mb-4' src="./section.png" alt="effect4" height={350} width={500}></img>
+            </div>
+            <div class="col-sm-6 rounded mt-5 apply">
+            <h3>Apply On Page</h3>
+            <p class="transparant">Take your website to new heights with Motion Art for Elementor. Embrace the power of motion and animation.</p>
+            <img class='mb-4' src="./page.png" alt="effect4" height={350} width={500}></img>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center mt-5 rounded browser p-5">
+                    <h3>Supported by All Popular Browsers</h3>
+                    <p class="transparant mt-4">Rest assured, Motion Art is designed to be compatible with all major web browsers.</p>
+                    <img class='mt-3' src="./browser.png" alt="effect4" height={50} width={400}></img>
+            </div>
+        </div>
+        <h2 class="text-center mb-4 mt-5">An All-Round Plugin With Powerful Features</h2>
+        <p class="transparant text-center">Whether you're a seasoned web designer or just starting out, Motion Art for Elementor seamlessly integrates with the Elementor platform, providing you with a seamless and intuitive experience.</p>
+        
+        <div class="row">
+            <div class="col-sm-4 rounded feature p-5 text-center">
+            <img class='mt-3' src="./feature1.png" alt="effect4" height={100} width={200}></img>
+            <h3>Light Weight</h3>
+            <p class="transparant">Motion Art for Elementor is designed to be lightweight.</p>
+            </div>
+            <div class="col-sm-4 rounded feature p-5 text-center">
+            <img class='mt-3' src="./feature2.png" alt="effect4" height={100} width={200}></img>
+            <h3>100% Responsive</h3>
+            <p class="transparant">Create a consistent visual experience across all devices.</p>
+            </div>
+            <div class="col-sm-4 rounded feature p-5 text-center">
+            <img class='mt-3' src="./feature3.png" alt="effect4" height={100} width={200}></img>
+            <h3>User Friendly Interface</h3>
+            <p class="transparant">Ensure a smooth experience for both applicants and administrators.</p>
+            </div>
+        </div>
+</div>;
+};
+
+const ChildComponent3 = () => {
+    return <div className="container">
+     <footer>
+        <div class="row ">
+            <div class="col-sm-10 transparant">
+                <span>
+                © 2023 Copywrite. All rights reserved by QodeMatrix
+                </span>
+            </div>
+            <div class="col-sm-1 transparant">
+            <span>Documentation</span>
+            </div>
+            <div class="col-sm-1 transparant">
+                
+            <span><a href='https://support.qodematrix.com/login'>Support</a></span>
+            </div>
+        </div> 
+    </footer>
+  </div>;
+  };
+
+
+export { ChildComponent1, ChildComponent2,  ChildComponent3 };
